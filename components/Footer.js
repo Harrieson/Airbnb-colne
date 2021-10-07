0ac48fcb0dcd0e671b44ff7d5aacdb01bbf240cd
Harrie <harrie@harrie.M>
2021-10-07 15:54:11 +0300

    static build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>About</h5>
+                <p>How Airbnb Works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>Accessibility</h5>
+                <p>Accessibility</p>
+                <p>Not a real site</p>
+                <p>I'm cloning</p>
+                <p>Referrals accepted</p>
+                <p>harriemburu@gmail.com</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>Host</h5>
+                <p>&@harriemburu</p>
+                <p>Front End dev</p>
+                <p>Reactjs, Nextjs, Flutterjs</p>
+                <p>Hundreds of builds</p>
+                <p>Contact me now</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'>Support</h5>
+                <p>HHelp center</p>
+                <p>Trust and Safety</p>
+                <p>Immediate reach</p>
+                <p>Refer Me</p>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ next.config.js
@@ -0,0 +1,7 @@
+// hostname = 'https://links.papareact.com/qd3';
+
+module.exports = {
+  images: {
+    domains: ['links.papareact.com'],
+  },
+}
